39f6fc1a9e0efa19b79372a83630d1bffacfa6f8
Robert Corrêa dos Santos <robertcorreas@gmail.com>
2016-07-02 14:57:13 -0300

    authentication_controller.lua contempla a regra 2 de disciplina

--- src/controllers/authentication_controller.lua
+++ src/controllers/authentication_controller.lua
@@ -10,10 +10,21 @@ local M = {
 }
 
 M.metatable = {
+
+  --[[
+  Responsabilidade: Método para a rota de login
+  Pré-Condição: * -
+  Pós-Condição: * Retorna a página de login
+  ]]
   login = function(self, params)
     return view.render("login.html.elua")
   end,
-  
+
+  --[[
+  Responsabilidade: Método para a rota de autenticação
+  Pré-Condição: * Deve receber parametros da rota (senha, cookie, etc)
+  Pós-Condição: * Retorna a página raiz ou retorna página de senha incorreta caso a senha esteja incorreta
+  ]]
   authenticate = function(self, params)
     local auth = Authentication:new(params.cookie)
     if auth:login(params.password) then
@@ -22,13 +33,24 @@ M.metatable = {
       return view.render("login.html.elua", {args = {err = "Senha incorreta"}})
     end
   end,
-  
+
+
+  --[[
+  Responsabilidade: Método para a rota de logout
+  Pré-Condição: * Deve receber parametros da rota (cookie)
+  Pós-Condição: * Retorna a página raiz
+  ]]
   logout = function(self, params)
     local auth = Authentication:new(params.cookie)
     auth:logout()
     return view.redirect_to("/")
   end,
 
+  --[[
+  Responsabilidade: Método para a rota de informação do usuário
+  Pré-Condição: * Deve receber parametros da rota (cookie)
+  Pós-Condição: * Retorna um json informando se o usuário está logado ou não
+  ]]
   info = function(self, params)
     local auth = Authentication:new(params.cookie)
     return view.render_json{logged = auth:is_signedin()}
