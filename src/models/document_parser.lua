b894a416dbee87d4f5c2eafccc4cb7a3e31e3413
Robert Corrêa dos Santos <robertcorreas@gmail.com>
2016-07-02 14:35:34 -0300

    document_parser.lua contempla a regra 2 de disciplina

--- src/models/document_parser.lua
+++ src/models/document_parser.lua
@@ -1,4 +1,9 @@
 return {
+  --[[
+  Responsabilidade: Retorna o texto de um pdf
+  Pré-Condição: * Deve receber caminho para o pdf
+  Pós-Condição: * Retorna o texto contido no pdf
+  ]]
   get_text = function(self, path)
     local handle = io.popen("pdftotext '" .. path .. "' '-'")
     local content = handle:read("*a")
