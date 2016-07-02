2c893106b48eb9e7ef53e82c8db545dcb0374b0d
Robert Corrêa dos Santos <robertcorreas@gmail.com>
2016-07-02 14:06:06 -0300

    authentication.lua contempla a regra 2 de disciplina

--- src/models/authentication.lua
+++ src/models/authentication.lua
@@ -7,6 +7,12 @@ local M = {
 }
 
 M.metatable = {
+  --[[
+  Responsabilidade: Verifica se um usuário é capaz de efetuar login
+  Pré-Condição: * Deve receber a senha de acesso
+  Pós-Condição: * Caso possa efetuar login, cookie de acesso é criado
+                * Retorna true ou false se pode ou não efetuar login
+  ]]
   login = function(self, password)
     if password == "pes3pes3" then
       self.cookie:set({
@@ -18,14 +24,24 @@ M.metatable = {
       return false
     end
   end,
-  
+
+  --[[
+  Responsabilidade: Faz logout do usuário
+  Pré-Condição: * -
+  Pós-Condição: * invalida o cookie de login
+  ]]
   logout = function(self)
     self.cookie:set({
         key = "type",
         value = "anonymous",
     })
   end,
-  
+
+  --[[
+  Responsabilidade: Verifica se o usuário está logado através do cookie
+  Pré-Condição: * -
+  Pós-Condição: * Retorna true ou false se o usuário está logado ou não
+  ]]
   is_signedin = function(self)
     return self.cookie:get("type") == "user"
   end,
