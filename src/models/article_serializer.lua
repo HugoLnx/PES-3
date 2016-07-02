e9c2d5af9aa1529624b2418a016970511a9973d6
Robert Corrêa dos Santos <robertcorreas@gmail.com>
2016-07-02 13:54:19 -0300

    article_serializer.lua contempla a regra 2 de disciplina

--- src/models/article_serializer.lua
+++ src/models/article_serializer.lua
@@ -4,6 +4,16 @@ This modulo has the functions about the article serializer
 local utils = require "utils"
 local ConferenceSerializer = require "models/conference_serializer"
 
+--[[
+Responsabilidade: Monta um objeto com dados de artigo e demais dados para serem usados no html
+Pré-Condição: * Banco de dados deve estar online
+              * Deve receber um artigo com suas propriedades preenchidas
+Pós-Condição: * Caminho do download é fornecido
+              * Autores são convertidos em uma string onde são separados por vírgula
+              * caminho do html é formado usando o id do artigo
+              * Conferencia é adicionada ao objeto
+              * Retona o objeto
+]]
 local function data_for(article)
   local data = utils.merge(article:data(), {
     download_path = article:document_path(),
@@ -22,9 +32,8 @@ return {
   serialize_one = function(self, article)
     return data_for(article)
   end,
-  
+
   serialize_many = function(self, articles)
     return utils.map(articles, function(article) return data_for(article) end)
   end,
 }
-
