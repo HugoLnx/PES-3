d978a83eee30bd3522d2c113522d0ea8e6d82c0d
Robert Corrêa dos Santos <robertcorreas@gmail.com>
2016-07-02 14:02:04 -0300

    article.lua contempla a regra 2 de disciplina

--- src/models/article.lua
+++ src/models/article.lua
@@ -7,6 +7,11 @@ local path = require "pl.path"
 
 local ATTRIBUTES = {"id", "title", "abstract", "authors", "downloads", "quoting_rate", "document_text", "creation_date", "conference_id"}
 
+--[[
+Responsabilidade: Retorna dados do artigo baseado numa lista de atributos
+Pré-Condição: * Deve receber os dados de artigo
+Pós-Condição: * Retorna dados do artigo baseado numa lista de atributos
+]]
 local function to_data(values)
   local data = {}
   for _,attribute in ipairs(ATTRIBUTES) do
@@ -24,7 +29,12 @@ local M = {
     setmetatable(article, {__index = self.metatable})
     return article
   end,
-  
+
+  --[[
+  Responsabilidade: Retorna artigos a partir de uma lista de dados de artigo
+  Pré-Condição: * Deve receber objetos contendo dados de artigo
+  Pós-Condição: * Retorna todos os artigos criados a partir dos dados
+  ]]
   build_all = function(self, articles_data)
     local articles = {}
     for i,values in ipairs(articles_data) do
@@ -32,17 +42,32 @@ local M = {
     end
     return articles
   end,
-  
+
+  --[[
+  Responsabilidade: Retorna lista de dados de artigos a partir de uma lista de artigos
+  Pré-Condição: * Deve receber uma lista de artigos
+  Pós-Condição: * Retorna todos os dados de artigos convertidos a partir dos artigos
+  ]]
   data_of_all = function(self, articles)
     return utils.map(articles, function(article) return article:data() end)
   end,
 }
 
 M.metatable = {
+  --[[
+  Responsabilidade: Retorna somente os dados de artigo
+  Pré-Condição: * -
+  Pós-Condição: * Retorna somente os dados de artigo
+  ]]
   data = function(self)
     return to_data(self)
   end,
-  
+
+  --[[
+  Responsabilidade: Retorna caminho do arquivo associado ao artigo
+  Pré-Condição: * Deve existir um id válido
+  Pós-Condição: * Retorna caminho do arquivo associado ao artigo
+  ]]
   document_path = function(self)
     return path.join("articles", self.id .. ".pdf")
   end,
