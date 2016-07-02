746e5c693c68bd346a692531d0acb02e678f213b
Robert Corrêa dos Santos <robertcorreas@gmail.com>
2016-07-02 14:34:00 -0300

    conference.lua contempla a regra 2 de disciplina

--- src/models/conference.lua
+++ src/models/conference.lua
@@ -6,6 +6,12 @@ local path = require "pl.path"
 
 local ATTRIBUTES = {"id", "abbreviation", "name", "location", "month", "year", "editors", "creation_date"}
 
+
+--[[
+Responsabilidade: Retorna dados da conferência baseado numa lista de atributos
+Pré-Condição: * Deve receber os dados de conferência
+Pós-Condição: * Retorna dados de conferência baseado numa lista de atributos
+]]
 local function to_data(values)
   local data = {}
   for _,attribute in ipairs(ATTRIBUTES) do
@@ -21,7 +27,13 @@ local M = {
     setmetatable(conference, {__index = self.metatable})
     return conference
   end,
-  
+
+
+    --[[
+    Responsabilidade: Retorna conferências a partir de uma lista de dados de conferência
+    Pré-Condição: * Deve receber objetos contendo dados de conferência
+    Pós-Condição: * Retorna todas as conferências criadas a partir dos dados
+    ]]
   build_all = function(self, conferences_data)
     local conferences = {}
     for i,values in ipairs(conferences_data) do
@@ -29,13 +41,23 @@ local M = {
     end
     return conferences
   end,
-  
+
+  --[[
+  Responsabilidade: Retorna lista de dados de conferências a partir de uma lista de conferências
+  Pré-Condição: * Deve receber uma lista de conferências
+  Pós-Condição: * Retorna todos os dados de conferências convertidos a partir das conferências
+  ]]
   data_of_all = function(self, conferences)
     return utils.map(conferences, function(conference) return conference:data() end)
   end,
 }
 
 M.metatable = {
+  --[[
+  Responsabilidade: Retorna somente os dados de conferência
+  Pré-Condição: * -
+  Pós-Condição: * Retorna somente os dados de conferência
+  ]]
   data = function(self)
     return to_data(self)
   end
